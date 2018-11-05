db079da9c811017a322aded087462a7cde1c70bd
Dan Morriss <daniel.morriss@visionstream.com.au>
2018-11-05 22:24:24 +1100

    WIP.

--- glhello.c
+++ glhello.c
@@ -6,13 +6,35 @@
 
 /* Handler for window-repaint event. Call back when the window first appears and
  *    whenever the window needs to be re-painted. */
+
+
+#define checkImageWidth 64
+#define checkImageHeight 64
+static GLubyte checkImage[checkImageHeight][checkImageWidth][4];
+static GLuint texName;
+
+void makeCheckImage(void)
+{
+    int i, j, c;
+
+    for (i = 0; i < checkImageHeight; i++) {
+        for (j = 0; j < checkImageWidth; j++) {
+            c = ((((i&0x8)==0)^((j&0x8))==0))*255;
+            checkImage[i][j][0] = (GLubyte) c;
+            checkImage[i][j][1] = (GLubyte) c;
+            checkImage[i][j][2] = (GLubyte) c;
+            checkImage[i][j][3] = (GLubyte) 255;
+        }
+    }
+}
+
 void display() {
     glClearColor(0.0f, 1.0f, 0.0f, 1.0f); // Set background color to black and opaque
     glClear(GL_COLOR_BUFFER_BIT);         // Clear the color buffer
 
     // Draw a Red 1x1 Square centered at origin
     glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-    glColor3f(1.0f, 0.0f, 0.0f); // Red
+//    glColor3f(1.0f, 0.0f, 0.0f); // Red
     glTexCoord2f(0.0,0.0);
     glVertex2f(-0.85f, -0.85f);    // x, y
     glTexCoord2f(1.0,0.0);
@@ -28,7 +50,26 @@ void display() {
 
 /* Main function: GLUT runs as a console application starting at main()  */
 int main(int argc, char** argv) {
+    GLenum error;
     glutInit(&argc, argv);                 // Initialize GLUT
+
+    makeCheckImage();
+//    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    glGenTextures(1, &texName);
+    glBindTexture(GL_TEXTURE_2D, texName);
+    glEnable( GL_TEXTURE_2D );
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
+//                    GL_NEAREST);
+//    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+//                    GL_NEAREST);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, checkImageWidth,
+                 checkImageHeight, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                 checkImage);
+    error = glGetError();
+
     glutInitWindowSize(1440, 900);   // Set the window's initial width & height
     glutCreateWindow("OpenGL Setup Test"); // Create a window with the given title
 //    glutInitWindowSize(512, 512);   // Set the window's initial width & height
